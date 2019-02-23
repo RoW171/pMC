b3ad414ed5edbf85ab0f75cea66a19d7e4ac0a2e
r0w171 <robinweiland@gmx.de>
2019-02-23 15:15:58 +0100

    added main engine to the AudioEngine and a basic listener to master volume

--- pmc/resources/audio.py
+++ pmc/resources/audio.py
@@ -2,7 +2,10 @@ __author__ = "Robin 'r0w' Weiland"
 __date__ = "2019-02-23"
 __version__ = "0.0.1"
 
-from pyglet.media import Player, SourceGroup
+from pyglet.media import load, Player, SourceGroup
+from pyglet.media.drivers.base import AbstractAudioDriver
+from pyglet.lib import load_library
+import pyglet
 
 
 class AudioFileNotFound(FileNotFoundError): pass
@@ -29,6 +32,61 @@ class AmbientPlayer(Player):
         super(AmbientPlayer, self).queue(looper)
 
     __call__ = queue
+
+    def __iadd__(self, other):
+        self.volume += other
+        return self
+
+    def __isub__(self, other):
+        self.volume -= other
+        return self
+
+
+class AudioEngine(dict):
+    def __init__(self, audioPath, avbinPath=None):
+        super(AudioEngine, self).__init__()
+        self.audioPath = audioPath
+        if avbinPath:
+            load_library(str(avbinPath))
+            pyglet.has_avbin = True
+        self._volume = 1.0
+        self.listener = AbstractAudioDriver.get_listener()
+
+        self.ambientPlayer = AmbientPlayer(self)
+
+    def __call__(self, item):
+        try: self[item].play()
+        except (KeyError,): raise AudioFileNotFound("'{}' was not loaded into the audioengine".format(item))
+
+    def __repr__(self): return 'AudioEngine, volume: {}'.format(self.volume)
+
+    __str__ = __repr__
+
+    def __float__(self): return self.volume
+
+    def __delitem__(self, key): self[key].delete()
+
+    def append(self, name, item): self[name] = load(str(item), streaming=False)
+
+    def load(self):
+        for name, file, in self.audioPath.get().items(): self.append(name, file)
+
+    @property
+    def volume(self): return self._volume
+
+    @volume.setter
+    def volume(self, nVolume):
+        if 0 < nVolume < 1:
+            self._volume = nVolume
+            self.listener.volume = self._volume
+
+    def __iadd__(self, other):
+        self.volume += other
+        return self
+
+    def __isub__(self, other):
+        self.volume -= other
+        return self
 
 
 if __name__ == '__main__': pass
