d5edc2336cdc552f01ee970f4af4732c644092de
r0w171 <robinweiland@gmx.de>
2019-02-24 22:57:31 +0100

    added very, very simple base class for singletons

---
+++ pmc/utils/singleton.py
@@ -0,0 +1,13 @@
+__author__ = "Robin 'r0w' Weiland"
+__date__ = "2019-02-24"
+__version__ = "0.0.1"
+
+
+class Singleton:
+    _shared_data = {}
+
+    def __init__(self):
+        self.__dict__ = self._shared_data
+
+
+if __name__ == '__main__': pass
