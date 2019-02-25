8865b8983f5b8309fcb8c40e02637beb5b5907e3
r0w171 <robinweiland@gmx.de>
2019-02-25 19:22:41 +0100

    engine preload for audio_drivers and gl_debug

--- pmc/engine/__init__.py
+++ pmc/engine/__init__.py
@@ -2,4 +2,16 @@ __author__ = "Robin 'r0w' Weiland"
 __date__ = "2019-02-25"
 __version__ = "0.0.1"
 
+from pathlib import Path
+
+# must run before any opengl or audio stuff
+from pyglet import options
+with Path('./res/data/internal').open('r') as internal: _data = ''.join(internal.readlines()).split('\n')
+gl_debug = bool(int(_data.pop(0)))
+audio_drivers = tuple(_data)
+
+options['audio'] = audio_drivers
+options['debug_gl'] = gl_debug
+
+
 if __name__ == '__main__': pass
