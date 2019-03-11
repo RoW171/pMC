f825496974c5acd322b8032dd556c371c802bf09
r0w171 <robinweiland@gmx.de>
2019-03-11 16:38:18 +0100

    reworked update system

--- pmc/utils/update.py
+++ pmc/utils/update.py
@@ -6,6 +6,7 @@ from progressbar import ProgressBar
 from urllib.request import urlopen, urlretrieve
 from urllib.error import URLError
 from os import system, remove, walk
+from sys import exit
 from shutil import rmtree, copytree, copy2
 from pathlib import Path
 from pmc.resources.io.serializer import json
@@ -16,8 +17,7 @@ try:
 except (ImportError,): compression = ZIP_STORED
 
 
-VERSIONFILE = Path('./res/data/version')
-REPO = 'https://raw.github.com/RoW171/pMC/master/'
+VERSIONFILEPATH = Path('./res/data/version')
 
 
 class UpdateError:
@@ -26,24 +26,24 @@ class UpdateError:
     def cleanup(self): pass
 
 
-def pause(): system('pause')
+def pause(): input('\nPress Return to continue...')  # system('pause')
 
 
 def clear(): system('cls')
 
 
-def replace(path, target):
-    if not isinstance(path, Path): path = Path(path)
+def replace(source, target):
+    if not isinstance(source, Path): source = Path(source)
     if not isinstance(target, Path): target = Path(target)
 
-    if not path.exists(): FileNotFoundError("path '{}' does not exist".format(path))
+    if not source.exists(): FileNotFoundError("path '{}' does not exist".format(source))
     if not target.exists(): FileNotFoundError("target '{}' does not exist".format(target))
 
     if not target.is_file(): rmtree(target)
     else: remove(target)
 
-    if not path.is_file(): copytree(str(path), str(target))
-    else: copy2(str(path), str(target))
+    if not source.is_file(): copytree(str(source), str(target))
+    else: copy2(str(source), str(target))
 
 
 def showprogress(block_num, block_size, total_size):
@@ -55,12 +55,12 @@ def showprogress(block_num, block_size, total_size):
     
     
 def getVersionFile():
-    try: return json.loadFile(VERSIONFILE)
+    try: return json.loadFile(VERSIONFILEPATH)
     except (Exception,): return None
     
 
 def getLatestVersion():
-    try: response = urlopen(REPO + 'res/data/version').read().decode('utf-8')
+    try: response = urlopen(VERSIONFILE['urls']['version']).read().decode('utf-8')
     except (URLError,): return -2
     except (ValueError,): return -1
     else:
@@ -72,100 +72,72 @@ def getLatestVersion():
 
 
 def getInstalledVersion():
-    if versionfile is None: return -1
-    release = versionfile['version']['release']
-    major, minor, patch, = versionfile['version']['version']
-    date = versionfile['meta']['date']
+    if VERSIONFILE is None: return -1
+    release = VERSIONFILE['version']['release']
+    major, minor, patch, = VERSIONFILE['version']['version']
+    date = VERSIONFILE['meta']['date']
     return release, major, minor, patch, date,
 
 
-def check():
+def update():
+    print('-' * 10, 'Updater', '-' * 10, sep='')
+    print('Checking for available updates...')
     installed = getInstalledVersion()
     latest = getLatestVersion()
+    print('\tInstalled on your system:', installed, '' if not isinstance(installed, int) else '[ERROR]')
+    print('\tLatest release available:', latest, '' if not isinstance(latest, int) else '[ERROR]', '\n')
+    if installed == -1:
+        print('There seems to be a problem with your installation. It is recommended to update.')
+        pause()
+    elif latest == -2:
+        print('Could not connect to the server. Consider checking your internet connection and try again.')
+        pause()
+    elif latest == -1:
+        print('Release-specific data from the server was received but corrupted.')
+        pause()
+    else:
+        if installed[0] < latest[0]:
+            print('There is an update available.')
+            if input('\tDo you want to update [Y/n]: ').lower() in ['yes', 'y', '']: runDownload(latest[0])
+            else: exit(0)
+        else:
+            print('Your game is up-to-date. No updates needed for now.')
+            pause()
 
-    if isinstance(installed, int):
-        pass  # error
-    if isinstance(latest, int):
-        if latest == -2:
-            pass  # error: network
-        elif latest == -1:
-            pass  # error
 
-    if installed[0] < latest[0]: return latest, installed
-    else: return
-
-
-def update():
-    print(getLatestVersion())
-    print(getInstalledVersion())
-
-
-def download(release):
+def runDownload(release):
     clear()
     directory = Path('./download_{}'.format(release))
     releasefile = directory / 'version_{}'.format(release)
-    #
+    print('create temporary directory')
     try: directory.mkdir()
     except (FileExistsError,): pass
-    #
-    urlretrieve(versionfile['url']['release'] + 'release_{}'.format(release), releasefile, showprogress)
-    #
+    print('downloading latest release')
+    urlretrieve(VERSIONFILE['url']['release'] + 'release_{}'.format(release), releasefile, showprogress)
+    print('\nextracting updates')
     zf = ZipFile(str(releasefile), 'r')
     extProgress = ProgressBar()
     for file in extProgress(zf.namelist()): zf.extract(file)
     del zf
+    print('extrated successfully')
+
     #
-    return release, directory, releasefile
+
+    print('installing')
+    fileProgress, folderProgress, = ProgressBar(), ProgressBar()
+    for dirpath, dirnames, filenames, in walk(directory):
+        print('installing files')
+        for file in fileProgress(filenames):
+            if file == releasefile.name: continue
+            replace(file.resolve(), Path(file).resolve())
+        print('installing directories')
+        break
+    print('installed successfully')
+    rmtree(str(directory))
+    pause()
 
 
-def install(release, dir, rfile):
-        print('installing')
-        fileProgress, folderProgress, = ProgressBar(), ProgressBar()
-        for dirpath, dirnames, filenames, in walk(dir):
-            print('installing files')
-            for file in fileProgress(filenames):
-                if file == rfile.name: continue
-                replace(file.resolve(), Path(file).resolve())
-            print('installing directories')
-            # for directory in folderProgress(dirnames):
-                # print(directory)
-                # replace(abspath(join(folder, directory)), abspath(directory))
-            break
-        print('installed successfully')
-        rmtree(dir)
-        pause()
+VERSIONFILE = getVersionFile()
+progress = None
 
-
-def create_version_file(version, release, notes='', state='stable'):
-    from datetime import datetime
-    date = datetime.now().date()
-    info = {
-        'meta': {
-            'author': 'Robin \'r0w\' Weiland',
-            'date': str(date.year) + '-' + str(date.month) + '-' + str(date.day)
-        },
-        'version': {
-            'version': version,
-            'release': release,
-            'notes': notes,
-            'state': state
-        },
-        'paths': {
-            'installed-version': str(VERSIONFILE)
-        },
-        'urls': {
-            'repo': REPO,
-            'version': REPO + 'res/data/version',
-            'release': REPO + 'release/'
-        }
-
-    }
-    json.dumpFile(Path('../../res/data/version'), info, indent=4)
-
-
-if __name__ == '__main__':
-    versionfile = getVersionFile()
-    # create_version_file((0, 0, 1,), 0, 'development - unstable')
-    progress = None
-    update()
-
+if __name__ == '__main__': pass
