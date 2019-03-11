494e3089ac8bd5ec162ca0ae7ec0bc0960891195
r0w171 <robinweiland@gmx.de>
2019-03-11 16:37:29 +0100

    more devtools

--- pmc/utils/devtools.py
+++ pmc/utils/devtools.py
@@ -2,4 +2,72 @@ __author__ = "Robin 'r0w' Weiland"
 __date__ = "2019-03-10"
 __version__ = "0.0.1"
 
+from ..resources.io.serializer import json, pickle
+from pathlib import Path
+from time import sleep
+from datetime import datetime
+from random import Random
+from os import walk
+
+
+class update:
+    REPO = 'https://raw.github.com/RoW171/pMC/master/'
+    VERSIONFILE = Path('../../res/data/version')
+
+    @staticmethod
+    def create_version_file(version, notes='', state='stable'):
+        date = datetime.now().date()
+        info = {
+            'meta': {
+                'author': __author__,
+                'date': str(date.year) + '-' + str(date.month) + '-' + str(date.day)
+            },
+            'version': {
+                'version': version,
+                'release': update.get_release_count() + 1,
+                'notes': notes,
+                'state': state
+            },
+            'paths': {
+                'installed-version': str(update.VERSIONFILE)
+            },
+            'urls': {
+                'repo': update.REPO,
+                'version': update.REPO + 'res/data/version',
+                'release': update.REPO + 'release/'
+            }
+
+        }
+        json.dumpFile(update.VERSIONFILE, info, indent=4)
+
+    @staticmethod
+    def get_release_count(directory='../../release'):
+        suffix = ''
+        release_number = -1
+        for root, subs, files, in walk(directory):
+            for file in files:
+                temp_rel_number = int(file.split('_')[-1].replace(suffix, ''))
+                if temp_rel_number > release_number: release_number = temp_rel_number
+            break
+        return release_number
+
+    @staticmethod
+    def pack_release(): pass
+
+
+class rng:
+    @staticmethod
+    def create_new_pool(file, size=10):
+        if not isinstance(file, Path): file = Path(file)
+        if file.exists():
+            pass  # TODO: warn here
+        r = Random()
+        l = []
+        for _ in range(size):
+            r.seed(datetime.now())
+            l.append(r.getstate())
+            sleep(1)
+        pickle.dumpFile(file, l)
+
+
 if __name__ == '__main__': pass
