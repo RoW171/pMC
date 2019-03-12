30499b4eb005d4fa7ce276fc66a885ce61bfff20
r0w171 <robinweiland@gmx.de>
2019-03-12 14:11:05 +0100

    added Keyboard and Mouse, but only templates, no actual functions

--- pmc/engine/peripherals/keyboard.py
+++ pmc/engine/peripherals/keyboard.py
@@ -2,4 +2,27 @@ __author__ = "Robin 'r0w' Weiland"
 __date__ = "2019-03-12"
 __version__ = "0.0.1"
 
+from pmc.engine.peripherals import PeripheralDevice
+from pyglet.window.key import *
+from pyglet.window.key import _1, _2, _3, _4, _5, _6, _7, _8, _9, _0
+
+numkeys = [_1, _2, _3, _4, _5, _6, _7, _8, _9, _0]
+numpadkeys = [NUM_1, NUM_2, NUM_3, NUM_4, NUM_5, NUM_6, NUM_7, NUM_8, NUM_9, NUM_0]
+
+
+commands_default = (
+    ('press', {}, ''),
+    ('release', {}, ''),
+)
+
+
+class KeyBoard(KeyStateHandler, PeripheralDevice):
+    def __init__(self, game, commands=commands_default):
+        super(KeyBoard, self).__init__(game, commands)
+
+    def on_key_press(self, symbol, modifiers): self['press'][self.press](symbol, modifiers, self.game)
+
+    def on_key_release(self, symbol, modifiers): self['release'][self.release](symbol, modifiers, self.game)
+
+
 if __name__ == '__main__': pass
--- pmc/engine/peripherals/mouse.py
+++ pmc/engine/peripherals/mouse.py
@@ -2,4 +2,34 @@ __author__ = "Robin 'r0w' Weiland"
 __date__ = "2019-03-12"
 __version__ = "0.0.1"
 
+from pmc.engine.peripherals import PeripheralDevice
+from pyglet.window.mouse import *
+from pyglet.window.key import MOD_CTRL
+
+
+commands_default = (
+    ('press', {}, ''),
+    'release',
+    ('motion', {}, ''),
+    ('scroll', {}, ''),
+)
+
+
+class Mouse(PeripheralDevice):
+    def __init__(self, game, commands=commands_default):
+        super(Mouse, self).__init__(game, commands)
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        self['press'][self.press](x, y, button, modifiers, self.game)
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        self['release'][self.release](x, y, button, modifiers, self.game)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self['motion'][self.motion](x, y, dx, dy, self.game)
+
+    def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
+        self['scroll'][self.scroll](x, y, scroll_x, scroll_y, self.game)
+
+
 if __name__ == '__main__': pass
