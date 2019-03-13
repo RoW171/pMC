20f67e213880278b5931e393ed7268780b888794
r0w171 <robinweiland@gmx.de>
2019-03-13 17:38:54 +0100

    added engine core

--- pmc/engine/__init__.py
+++ pmc/engine/__init__.py
@@ -6,12 +6,51 @@ from pathlib import Path
 
 # must run before any opengl or audio stuff
 from pyglet import options
-with Path('./res/data/internal').open('r') as internal: _data = ''.join(internal.readlines()).split('\n')
+with Path('./res/data/internal').open('r') as internal:
+    _data = list(map(lambda line: line.strip(), internal.readlines()))
+
 gl_debug = bool(int(_data.pop(0)))
 audio_drivers = tuple(_data)
 
 options['audio'] = audio_drivers
 options['debug_gl'] = gl_debug
+
+from atexit import register
+from pmc.resources import Ressources
+from pmc.resources.settings import Settings
+from pmc.resources.audio import AudioEngine
+from pmc.resources.textures import TextureEngine
+
+
+class Engine:
+    def __init__(self):
+        register(self.cleanup)
+        self.closemessage = []
+
+        self.resources = Ressources(Path('./res'))
+        self.data = Settings(self.resources.data.settings)
+
+        if 'client' not in self.__dict__: self.client = False
+
+        self.window = None
+        self.renderer = None
+        if self.client: self.audio = AudioEngine(self.resources.audio, self.resources.libs.avbin)
+        else: self.audio = None
+        self.textures = TextureEngine(self.resources.textures, self.resources.img)
+        self.world = None
+        self.peripherals = None
+
+    def cleanup(self, message=None):
+        if message is None: message = self.closemessage
+        print('-' * 10, 'cleanup', '-' * 10, sep='')
+        for m in message: print(m, end='\n\n')
+        # TODO: show average fps here later
+        del self.resources
+        del self.data
+        del self.audio
+        del self.textures
+        del self.window
+        del self.peripherals
 
 
 if __name__ == '__main__': pass
