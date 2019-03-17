ddd41c57ca8fb100845ba6e2736d1c04bd5191af
r0w171 <robinweiland@gmx.de>
2019-03-17 20:52:22 +0100

    changed settings files from config to json

--- pmc/resources/settings.py
+++ pmc/resources/settings.py
@@ -2,13 +2,25 @@ __author__ = "Robin 'r0w' Weiland"
 __date__ = "2019-02-24"
 __version__ = "0.0.1"
 
-from pmc.resources.io.config import Config
+from pmc.resources.io.serializer import json
+
+
+class SettingsFile(dict):
+    def __init__(self, file):
+        self._file = file
+        super(SettingsFile, self).__init__(self.load())
+
+    def __getattribute__(self, item): return super(SettingsFile, self).__getitem__(item)
+
+    def load(self): return self._file.json()
+
+    def save(self): self._file.json(self.copy())
 
 
 class Settings:
     def __init__(self, settingsPath):
         for name, file, in settingsPath.get().items():
-            self.__dict__[name] = Config(file)
+            self.__dict__[name] = SettingsFile(json.loadFile(file))
 
     def __len__(self): return len(self.__dict__)
 
